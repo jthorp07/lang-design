97ad0023f350419786a308fe74dd12908ed5bb19
Jack Thorp <jackthorp07@gmail.com>
2025-02-25 18:52:42 -0500

    Add tokenizer to step_one.cpp and build process

--- step_one_tokenizer/src/step_one.cpp
+++ step_one_tokenizer/src/step_one.cpp
@@ -4,7 +4,19 @@
  * @brief Driver file to run a demo of the project reflecting the progress made in step one.
  */
 
+#include <iostream>
+#include <vector>
+#include "tokenizer.hpp"
+
 int main() {
+
+    imperium_lang::Tokenizer tokenizer{"step_one_test.imp"};
+    std::vector<imperium_lang::Token> tokens{};
+    const auto status = tokenizer.tokenize(tokens);
+    if (status != 0) {
+        std::cerr << "Error: Tokenization failed.\n";
+        return status;
+    }
 
     return 0;
 }

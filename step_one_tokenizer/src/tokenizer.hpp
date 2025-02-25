ef83305ba44f487346de89d8dbc03771ed59752e
Jack Thorp <jackthorp07@gmail.com>
2025-02-25 17:08:52 -0500

    Add Tokenizer class declaration

---
+++ step_one_tokenizer/src/tokenizer.hpp
@@ -0,0 +1,67 @@
+/**
+ * @file tokenizer.hpp
+ * 
+ * @brief Include file for tokenizer class
+ */
+
+#ifndef TOKENIZER_HPP
+#define TOKENIZER_HPP
+
+#include <string>
+#include <array>
+
+namespace imperium_lang {
+
+    constexpr int BLOCK_SIZE = 4096;
+    constexpr int BUFFER_SIZE = BLOCK_SIZE * 16 * 16;
+
+    /**
+     * @brief Tokenizer class
+     * 
+     * This class is responsible for tokenizing the content of a source file.
+     */
+    class Tokenizer {
+    private:
+        const std::string sourceFile;
+        std::array<char, BUFFER_SIZE> buffer;
+    public:
+        /**
+         * @brief Constructor
+         * 
+         * @param[in] sourceFile The source file to tokenize
+         */
+        Tokenizer(const std::string& sourceFile);
+
+        /**
+         * @brief Destructor
+         * 
+         * Clean up internal buffer and file stream
+         */
+        ~Tokenizer();
+
+        /**
+         * @brief Tokenize the source file
+         * 
+         * @param[out] tokens The tokens extracted from the source file
+         * @return Status code
+         * @retval 0 Success
+         * @retval -1 Parse Error: Failed to create a token
+         * @retval -2 Read Error
+         */
+        int tokenize(auto& tokens);
+    private:
+        /**
+         * @brief Refills the internal buffer with the next content from the source
+         *        file, if the file has not already been fully read
+         * 
+         * @param[out] bytesRead Number of bytes read
+         * @return Status code
+         * @retval 0 Success
+         * @retval -1 Read Error
+         */
+        int refillBuffer(int bytesRead);
+    };
+
+}
+
+#endif
